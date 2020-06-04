85dcce530d4d1118d4d0c3950a6df7b7b93a77e8
leehj00 <astralrrrrr@gmail.com>
2020-06-04 17:29:58 +0900

    sear

--- BUBBLEBEE/src/main/java/com/kh/bubblebee/board/controller/BoardSearchController.java
+++ BUBBLEBEE/src/main/java/com/kh/bubblebee/board/controller/BoardSearchController.java
@@ -1,12 +1,19 @@
 package com.kh.bubblebee.board.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.kh.bubblebee.board.model.exception.BoardException;
 import com.kh.bubblebee.board.model.service.BoardService;
+import com.kh.bubblebee.board.model.vo.Board;
 import com.kh.bubblebee.board.model.vo.SearchCondition;
+import com.kh.bubblebee.common.PageInfo;
+import com.kh.bubblebee.common.Pagination;
 
 public class BoardSearchController {
 	
@@ -14,7 +21,7 @@ public class BoardSearchController {
 	private BoardService bService;
 	
 	@RequestMapping("search.bo")
-	public ModelAndView searchAll(@RequestParam(value="a") String a, @RequestParam(value="startPrice") int startPrice, @RequestParam(value="endPrice") int endPrice, ModelAndView mv) {
+	public ModelAndView searchAll(@RequestParam(value="page", required=false) Integer page, @RequestParam(value="cate") String cate, @RequestParam(value="a", required=false) String a, @RequestParam(value="startPrice") int startPrice, @RequestParam(value="endPrice") int endPrice, ModelAndView mv) {
 		
 		SearchCondition sc = new SearchCondition();
 		
@@ -38,6 +45,33 @@ public class BoardSearchController {
 			}
 		}
 		
+		int currentPage = 1;
+		if(page!=null) {
+			currentPage = page;
+		}
+		
+		int listCount = bService.getSearchListCount(sc);
+		PageInfo pi = Pagination.getPageInfo(currentPage, listCount);
+		
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("cate", cate);
+		map.put("sc", sc);
+		
+		ArrayList<Board> list = bService.selectSearchList(map, pi);
+		
+		
+		if(list != null) {
+			mv.addObject("cate", cate);
+			mv.addObject("list", list);
+			mv.addObject("pi", pi);
+			mv.addObject("a", a);
+			mv.addObject("startPrice", startPrice);
+			mv.addObject("endPrice", endPrice);
+			mv.setViewName("listView_search");
+			
+		} else {
+			throw new BoardException("필터 조회에 실패했습니다.");
+		}
 		
 		return mv;
 	}
--- BUBBLEBEE/src/main/java/com/kh/bubblebee/board/model/service/BoardService.java
+++ BUBBLEBEE/src/main/java/com/kh/bubblebee/board/model/service/BoardService.java
@@ -1,9 +1,11 @@
 package com.kh.bubblebee.board.model.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.kh.bubblebee.board.model.vo.Board;
 import com.kh.bubblebee.board.model.vo.Review;
+import com.kh.bubblebee.board.model.vo.SearchCondition;
 import com.kh.bubblebee.common.PageInfo;
 
 public interface BoardService {
@@ -17,5 +19,9 @@ public interface BoardService {
 	ArrayList<Board> selectHtList(String cate);
 
 	int insertBoard(Board b);
+
+	int getSearchListCount(SearchCondition sc);
+
+	ArrayList<Board> selectSearchList(HashMap<String, Object> map, PageInfo pi);
 	
 }
