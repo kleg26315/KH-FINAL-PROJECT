5b7e8d5b0c8f633fa3fbfac420ab52a92b938643
김경섭(Kim KyouongSub) <kleg26315@naver.com>
2020-06-05 21:11:59 +0900

    xls 문서화, 공지사항 조회 수정 완료

--- BUBBLEBEE/src/main/java/com/kh/bubblebee/notice/controller/NoticeController.java
+++ BUBBLEBEE/src/main/java/com/kh/bubblebee/notice/controller/NoticeController.java
@@ -1,14 +1,21 @@
 package com.kh.bubblebee.notice.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.kh.bubblebee.board.model.exception.BoardException;
+import com.kh.bubblebee.common.PageInfo;
+import com.kh.bubblebee.common.Pagination;
 import com.kh.bubblebee.notice.model.exception.NoticeException;
 import com.kh.bubblebee.notice.model.service.NoticeService;
+import com.kh.bubblebee.notice.model.vo.Notice;
 
 @Controller
 public class NoticeController {
@@ -17,8 +24,28 @@ public class NoticeController {
 	private NoticeService nService;
 	
 	@RequestMapping("list.no")
-	public String tpmoimList() {
-		return "noticeList";
+	public ModelAndView noticeList(@RequestParam(value="page", required=false) Integer page, ModelAndView mv) {
+		int currentPage = 1;
+		if(page != null) {
+			currentPage = page;
+		}
+		
+		int listCount = nService.getNoticeListCount();
+		
+		PageInfo pi = Pagination.getPageInfo(currentPage, listCount);
+		
+		ArrayList<Notice> list = nService.selectNoticeList(pi);
+		
+		if(list != null) {
+			// list, pi, view
+			mv.addObject("list", list);
+			mv.addObject("pi", pi);
+			mv.setViewName("noticeList");
+		} else {
+			throw new NoticeException("게시글 전체 조회에 실패했습니다.");
+		}
+		
+		return mv;
 	}
 	
 	@RequestMapping(value="insert.no", method = RequestMethod.POST)
@@ -32,4 +59,19 @@ public class NoticeController {
 			throw new NoticeException("공지사항 등록에 실패하였습니다.");
 		}
 	}
+	
+	@RequestMapping(value="insertFAQ.no", method = RequestMethod.POST)
+	public String insertFAQ(@RequestParam("title") String title, @RequestParam("content") String content,
+							@RequestParam("btype") int btype, Model model) {
+		
+		int result = nService.insertFAQ(title, content, btype);
+		
+		if(result > 0) {
+			return "redirect:list.no";
+		} else {
+			throw new NoticeException("공지사항 등록에 실패하였습니다.");
+		}
+	}
+	
+	
 }
--- BUBBLEBEE/src/main/java/com/kh/bubblebee/notice/model/dao/NoticeDAO.java
+++ BUBBLEBEE/src/main/java/com/kh/bubblebee/notice/model/dao/NoticeDAO.java
@@ -1,9 +1,14 @@
 package com.kh.bubblebee.notice.model.dao;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.kh.bubblebee.common.PageInfo;
+import com.kh.bubblebee.notice.model.vo.Notice;
 
 @Repository("nDAO")
 public class NoticeDAO {
@@ -13,6 +18,25 @@ public class NoticeDAO {
 		map.put("title", title);
 		map.put("content", content);
 		return sqlSession.insert("noticeMapper.insertNotice", map);
+	}
+
+	public int insertFAQ(SqlSessionTemplate sqlSession, String title, String content, int btype) {
+		HashMap<String, String> map = new HashMap<>();
+		map.put("title", title);
+		map.put("content", content);
+		map.put("btype", btype+"");
+		return sqlSession.insert("noticeMapper.insertFAQ", map);
+	}
+
+	public int getNoticeListCount(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("noticeMapper.getNoticeListCount");
+	}
+
+	public ArrayList<Notice> selectNoticeList(SqlSessionTemplate sqlSession, PageInfo pi) {
+		int offset = (pi.getCurrentPage() -1 ) * pi.getBoardLimit();
+		RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
+		
+		return (ArrayList)sqlSession.selectList("noticeMapper.selectNoticeList", null, rowBounds);
 	}
 
 }
