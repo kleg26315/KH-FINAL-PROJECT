7c5e3ca882a2b9583f2ee6fd1de3173cfa5ba1ed
jhAg047 <jhag047@gmail.com>
2020-06-11 16:45:26 +0900

    header/mypage-header/프로필 사진 변경

--- BUBBLEBEE/src/main/java/com/kh/bubblebee/member/model/service/MypageService.java
+++ BUBBLEBEE/src/main/java/com/kh/bubblebee/member/model/service/MypageService.java
@@ -1,10 +1,15 @@
 package com.kh.bubblebee.member.model.service;
 
+import java.util.HashMap;
+
 import com.kh.bubblebee.member.model.vo.Member;
 
 public interface MypageService {
 
 	// 내 정보 수정
 	int updateInfo(Member m);
+
+	// 프로필 바꾸기
+	int updateprofile(HashMap<String, String> map);
 	
 }
