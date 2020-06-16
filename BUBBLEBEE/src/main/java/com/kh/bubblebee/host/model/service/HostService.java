40f3c28db76a83a2007e2e3be2afa8867c89a53d
jhAg047 <jhag047@gmail.com>
2020-06-17 03:46:38 +0900

    boardDetail에서 문의하기/호스트 프로필 보기 수정중!

--- BUBBLEBEE/src/main/java/com/kh/bubblebee/host/model/service/HostService.java
+++ BUBBLEBEE/src/main/java/com/kh/bubblebee/host/model/service/HostService.java
@@ -1,5 +1,10 @@
 package com.kh.bubblebee.host.model.service;
 
+import java.util.ArrayList;
+
+import com.kh.bubblebee.board.model.vo.Board;
+import com.kh.bubblebee.board.model.vo.Review;
+import com.kh.bubblebee.common.PageInfo;
 import com.kh.bubblebee.host.model.vo.Host;
 import com.kh.bubblebee.member.model.vo.Member;
 
@@ -11,4 +16,23 @@ public interface HostService {
 	// 호스트 정보 추가하기
 	int enrollHost(Host h);
 
+	// 호스트 정보 가져오기
+	Host selectHost(String hostId);
+	
+	// 해당 호스트가 작성한 게시글 개수 가져오기
+	int getListCount(String hostId);
+
+	// 해당 호스트가 작성한 게시글 가져오기
+	ArrayList<Board> selectBoard(PageInfo pi, String hostId);
+
+	// 문의글 작성
+	int insertHostQnA(Review r);
+
+	// QnA 개수 
+	int selectQnACount(int fno);
+
+	// QnA 목록
+	ArrayList<Review> selectQnAList(PageInfo pi, int fno);
+
+	
 }
--- BUBBLEBEE/src/main/java/com/kh/bubblebee/host/model/service/HostServiceImpl.java
+++ BUBBLEBEE/src/main/java/com/kh/bubblebee/host/model/service/HostServiceImpl.java
@@ -1,10 +1,15 @@
 package com.kh.bubblebee.host.model.service;
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.bubblebee.board.model.vo.Board;
+import com.kh.bubblebee.board.model.vo.Review;
+import com.kh.bubblebee.common.PageInfo;
 import com.kh.bubblebee.host.model.dao.HostDAO;
 import com.kh.bubblebee.host.model.vo.Host;
 import com.kh.bubblebee.member.model.vo.Member;
@@ -27,5 +32,37 @@ public class HostServiceImpl implements HostService{
 	public int enrollHost(Host h) {
 		return hDAO.enrollHost(sqlSession,h);
 	}
+
+	@Override
+	public Host selectHost(String hostId) {
+		return hDAO.selectHost(sqlSession,hostId);
+	}
+
+	@Override
+	public int getListCount(String hostId) {
+		return hDAO.getListCount(sqlSession,hostId);
+	}
+
+	@Override
+	public ArrayList<Board> selectBoard(PageInfo pi, String hostId) {
+		return hDAO.selectBoard(sqlSession,pi,hostId);
+	}
+
+	@Override
+	public int insertHostQnA(Review r) {
+		return hDAO.insertHostQnA(sqlSession,r);
+	}
+
+	@Override
+	public int selectQnACount(int fno) {
+		return hDAO.selectQnACount(sqlSession,fno);
+	}
+
+	@Override
+	public ArrayList<Review> selectQnAList(PageInfo pi, int fno) {
+		return hDAO.selectQnAList(sqlSession,pi,fno);
+	}
+
+
 
 }
